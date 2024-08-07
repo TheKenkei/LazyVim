3b17a6b1913eb5110c9207e3d318a291160a8004
TheKenkei <nowikow.witaliy@gmail.com>
2024-08-08 02:01:00 +0300

    feat: add configs

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,30 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local map = vim.keymap.set
+
+map("n", "<c-l>", "<cmd>:TmuxNavigateRight<cr>", { desc = "Tmux Right" })
+map("n", "<c-h>", "<cmd>:TmuxNavigateLeft<cr>", { desc = "Tmux Left" })
+map("n", "<c-k>", "<cmd>:TmuxNavigateUp<cr>", { desc = "Tmux Up" })
+map("n", "<c-j>", "<cmd>:TmuxNavigateDown<cr>", { desc = "Tmux Down" })
+
+map("n", "<S-Tab>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+map("n", "<Tab>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
+map("n", "<leader>ss", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+map("n", "<leader>gg", function()
+  LazyVim.lazygit({ cwd = LazyVim.root.git() })
+end, { desc = "Lazygit (Root Dir)" })
+
+local lazyterm = function()
+  LazyVim.terminal(nil, { cwd = LazyVim.root() })
+end
+map("t", "<A-i>", "<C-D>", { desc = "Terminal (cwd)" })
+
+map("n", "<A-i>", lazyterm, { desc = "Terminal (Root Dir)" })
+map("n", "<A-I>", function()
+  LazyVim.terminal()
+end, { desc = "Terminal (cwd)" })
+
+map("n", "<C-a>", "ggVG", { desc = "Terminal (Root Dir)" })
+map("n", "<leader>dd", LazyVim.ui.bufremove, { desc = "Delete Buffer" })
+
+map("i", "<C-g>", function()
+  return vim.fn["codeium#Accept"]()
+end, { expr = true })
